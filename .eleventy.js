94933e5d8d7db2c7c3edeed8e0e5999576541dea
PHANUMASH PHUKAN <phanumash.alive@gmail.com>
2026-05-06 14:56:45 +0700

    Add 11ty + Decap CMS project structure

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  return {
+    dir: { input: "src", output: "public" }
+  };
+};
---
+++ src/projects/2026-05-06-hello-world.md
@@ -0,0 +1,8 @@
+---
+title: "Hello World"
+date: 2026-05-06
+thumbnail: /images/sample.png
+category: "Web Design"
+link: "https://github.com"
+---
+นี่คือรายละเอียดของผลงานชิ้นแรก **ใช้ Markdown ได้** 😊
